ded9c0a48e16274392d8d4c69a46f0ee6c99e0b8
Chace <chace@shipt.com>
2017-11-26 19:59:55 -0600

    fix anonymizing

--- app/services/dicom_service.rb
+++ app/services/dicom_service.rb
@@ -3,11 +3,13 @@ class DicomService
   def self.anonymize_directory(path)
     include DICOM
 
-    dicom_files = Dir.glob("#{path}/**/*.dcm")
+    dicom_files = Dir.glob("#{path}/**/*")
     dicom_files.each do |file_path|
       dicom_file = DObject.read(file_path)
-      dicom_file.anonymize
-      dicom_file.write(file_path)
+      unless dicom_file.print.empty?
+        dicom_file.anonymize
+        dicom_file.write(file_path)
+      end
     end
   end
 
