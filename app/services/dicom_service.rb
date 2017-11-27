c01d18c375eb0c7f4b59caa737b6d66c75cfebbc
Chace <chace@shipt.com>
2017-11-26 22:56:03 -0600

    manual anonymization to fix issue with images messing up

--- app/services/dicom_service.rb
+++ app/services/dicom_service.rb
@@ -7,10 +7,24 @@ class DicomService
     dicom_files.each do |file_path|
       dicom_file = DObject.read(file_path)
       unless dicom_file.print.empty?
-        dicom_file.anonymize
+        anonymize(dicom_file)
         dicom_file.write(file_path)
       end
     end
   end
 
+  private
+
+  def self.anonymize(dicom_file)
+    anonymize_field(dicom_file["0008,0080"], "Institution Name")
+    anonymize_field(dicom_file["0008,0081"], "Institution Address")
+    anonymize_field(dicom_file["0008,1070"], "Operator Name")
+    anonymize_field(dicom_file["0008,0090"], "Referring Physician")
+    anonymize_field(dicom_file["0010,0010"], "Patient Name")
+    anonymize_field(dicom_file["0010,0030"], "Patient D.O.B.")
+  end
+
+  def self.anonymize_field(field, new_value)
+    field.value = new_value if field.present?
+  end
 end
